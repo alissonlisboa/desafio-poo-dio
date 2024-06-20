dacd93beb1e90d4a5ee0c734aaa35bdce0c0d4cf
Lisboa, Alisson <alisson.lisboa@br.experian.com>
2024-06-19 22:51:24 -0300

    Seggregate 'calcularXp' behavior to an new Interface

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,17 +1,16 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso extends Conteudo{
+public class Curso extends Conteudo {
 
     private int cargaHoraria;
 
     @Override
     public double calcularXp() {
-        return XP_PADRAO * cargaHoraria;
+        return Conteudo.XP_PADRAO * cargaHoraria;
     }
 
     public Curso() {
     }
-
 
     public int getCargaHoraria() {
         return cargaHoraria;
